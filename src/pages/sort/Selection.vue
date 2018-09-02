c0e95c544221e4cfc8bfc07e5b071246a08b7bfd
softech86 <softech86@163.com>
2018-09-02 22:11:54 +0800

    [+] selectable number

--- src/pages/sort/Selection.vue
+++ src/pages/sort/Selection.vue
@@ -1,21 +1,55 @@
 <template>
-    <div class="container">
-        <h2>选择排序</h2>
-        <transition-group name="flip-list" tag="div" class="list">
-            <div
-                class="item"
-                v-for="(item, _) in array"
-                :key="item"
-                :style="{height: item * 3 + 'px'}"
-                :class="{selected: _ === pointer.i || _ === pointer.j}"
-            >
-                <div class="value">{{ item }}</div>
+    <div class="parent">
+        <div class="array">
+            <div class="type">
+                <el-button icon="el-icon-sort-up" @click="sortUp" round>升序</el-button>
+                <el-button icon="el-icon-refresh" @click="sortRand" round>乱序</el-button>
+                <el-button icon="el-icon-sort-down" @click="sortDown" round>降序</el-button>
             </div>
-        </transition-group>
+            <div class="numbers">
+                <el-button
+                    @click="toggleArray(i)"
+                    round
+                    size="mini"
+                    v-for="i in 20"
+                    :key="`number-${i}`"
+                    :style="getStyle(i)"
+                    :type="array.indexOf(i) >= 0 ? 'primary' : ''"
+                >{{ i }}</el-button>
+            </div>
+        </div>
+        <div class="container">
+            <h2>选择排序</h2>
+            <div class="list" v-if="!step">
+                <div
+                    class="item"
+                    v-for="(item, _) in array"
+                    :key="item"
+                    :style="{height: item * 10 + 'px'}"
+                    :class="{selected: _ === pointer.i || _ === pointer.j}"
+                >
+                    <div class="value">{{ item }}</div>
+                </div>
+            </div>
+            <transition-group name="flip-list" tag="div" class="list" v-else>
+                <div
+                    class="item"
+                    v-for="(item, _) in array"
+                    :key="item"
+                    :style="{height: item * 10 + 'px'}"
+                    :class="{selected: _ === pointer.i || _ === pointer.j}"
+                >
+                    <div class="value">{{ item }}</div>
+                </div>
+            </transition-group>
 
-        <div class="btns">
-            <el-button icon="el-icon-d-arrow-right" @click="rush" round>Sort</el-button>
-            <el-button icon="el-icon-arrow-right" @click="next" round>Next</el-button>
+            <div class="btns">
+                <el-button icon="el-icon-d-arrow-right" @click="rush" round>Sort</el-button>
+                <el-button icon="el-icon-close" @click="pause" round>Pause</el-button>
+                <el-button icon="el-icon-arrow-right" @click="next" round>Next</el-button>
+            </div>
+        </div>
+        <div class="code" v-html="formattedCode">
         </div>
     </div>
 </template>
@@ -23,6 +57,10 @@
 <script>
 /* eslint-disable */
 import _ from 'lodash'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/tomorrow.css'
+
+window.hljs = hljs
 
 function delay(duration) {
     return new Promise(r => setTimeout(r, duration))
@@ -36,17 +74,66 @@ export default {
             duration: 1000,
             pointer: {i: -1, j: -1},
             step: 0,
-            stepLock: 0
+            stepLock: 0,
+            code: `
+for (let i = 0; i < len - 1; ++i) {
+    this.pointer = Object.assign(this.pointer, { i })
+    for (let j = i + 1; j < len; ++j) {
+
+        this.pointer = Object.assign(this.pointer, { j })
+        if (this.array[i] > this.array[j]) {
+            await this.swap(i, j)
+        } else {
+            await delay(this.duration / 4)
+        }
+        await this.nextStep()
+    }
+}`
         }
     },
     mounted () {
-        const s = new Set()
-        while (s.size < this.len) {
-            s.add(_.random(0, this.len * 10))
+        // const s = new Set()
+        // while (s.size < this.len) {
+        //     s.add(_.random(0, this.len * 10))
+        // }
+        // this.array = [...s]
+        // this.array = new Array(10).fill(0)
+    },
+    computed: {
+        formattedCode () {
+            return `<div>${
+                hljs.highlightAuto(this.code).value
+                    .replace(/\n/g, '</div><div>')
+                    .replace(/\s{4}/g, '<i style="display: inline-block;width: 20px;"></i>')
+            }</div>`
         }
-        this.array = [...s]
     },
     methods: {
+        getStyle (i) {
+            let top = i * 35 - 30 + 'px'
+            let left = 0
+
+            const index = this.array.indexOf(i)
+            if (index >= 0) {
+                top = index * 35 + 5 + 'px'
+                left = 100 + 'px'
+            }
+            return {
+                top,
+                left
+            }
+        },
+        toggleArray(i) {
+            const index = this.array.indexOf(i)
+            if (index >= 0) {
+                this.array.splice(index, 1)
+            } else {
+                if (this.array.length >= 10) {
+                    this.array.splice(0, 1)
+                }
+                this.array.push(i)
+            }
+        },
         async swap (i, j) {
             const t = this.array[i]
             this.$set(this.array, i, this.array[j])
@@ -83,6 +170,18 @@ export default {
             }
             this.pointer.i = this.pointer.j = this.len
         },
+        sortUp () {
+            this.array.sort((a, b) => a - b)
+            this.step = this.stepLock = 0
+        },
+        sortDown () {
+            this.array.sort((a, b) => b - a)
+            this.step = this.stepLock = 0
+        },
+        sortRand () {
+            this.array = _.shuffle(this.array)
+            this.step = this.stepLock = 0
+        },
         next () {
             if (!this.stepLock) {
                 this.step += 1
@@ -95,21 +194,56 @@ export default {
                 this.sort()
             }
             this.stepLock = Infinity
+        },
+        pause () {
+            this.stepLock = this.step
         }
     }
 }
 </script>
 
 <style scoped>
+    .parent {
+        height: 75vh;
+        display: flex;
+        justify-content: space-between;
+    }
+    .code {
+        text-align: left;
+    }
+
+    .array, .code, .numbers {
+        flex: 1;
+    }
+    .array {
+        display: flex;
+        flex-direction: column;
+    }
+    .numbers {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: space-between;
+        margin-top: 20px;
+        position: relative;
+        margin-left: 114px;
+    }
+    .numbers .el-button {
+        margin: 0;
+        position: absolute;
+        transition: .3s;
+    }
     .container {
         height: 75vh;
         display: flex;
         flex-direction: column;
         justify-content: space-evenly;
+        margin: 0 10px;
     }
     .list {
         display: flex;
         width: 400px;
+        height: 200px;
         margin: auto;
         justify-content: space-evenly;
         align-items: flex-end;
@@ -117,7 +251,10 @@ export default {
     .item {
         background: #caf1e0;
         width: 15px;
-        border-bottom: 40px solid #fbe59a;
+        border-bottom: 40px solid #65bf96;
+        color: #fff2c6;
+        font-weight: bolder;
+        text-shadow: 0 0 2px black;
         position: relative;
         box-shadow: 1px 1px 4px -3px black;
         transition: background-color .2s;
@@ -126,7 +263,7 @@ export default {
         transition: transform .6s, background-color .2s;
     }
     .selected {
-        background: #47e09b;
+        background: #fbe59a;
     }
     .value {
         position: absolute;

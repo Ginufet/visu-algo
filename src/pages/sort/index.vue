328777ef7e5c90318285d7149b0fa1f724b584a9
softech86 <softech86@163.com>
2018-09-02 22:41:55 +0800

    [+] return to home button

--- src/pages/sort/index.vue
+++ src/pages/sort/index.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        <h1>排序</h1>
+        <h1>
+            <span>排序</span>
+            <router-link :to="{name: 'HelloWorld'}" class="el-button el-button--default el-button--mini is-round">返回目录</router-link>
+        </h1>
         <router-view></router-view>
     </div>
 </template>
+
+<style scoped>
+    h1 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    a {
+        text-decoration: none;
+        margin: 4px 0 0 10px;
+    }
+</style>
